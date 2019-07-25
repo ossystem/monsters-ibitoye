caff398ede19ebc99b43349f86a28fc395f34c6d
Best <rotimiibitoyebest@yahoo.co.uk>
2019-07-25 18:43:55 +0300

    fix: add try again button

--- client/src/reducers/questionReducer.js
+++ client/src/reducers/questionReducer.js
@@ -1,14 +1,31 @@
-import { GET_QUESTION } from '../actions/types';
-import { QUESTION_AUTH } from "../helpers/constants";
+import { GET_QUESTION, RESET_QUESTION } from '../actions/types';
 
-const initialState = QUESTION_AUTH;
+const initialState = {
+  id: 0,
+  question: "",
+  optionType: "",
+  options: [
+    {
+      "id": 9,
+      "questionId": 3,
+      "chosenBy": null,
+      "option": ""
+    },
+    {
+      "id": 10,
+      "questionId": 3,
+      "chosenBy": null,
+      "option": "",
+    },
+  ],
+};
 
 export default function(state = initialState, action) {
   switch(action.type) {
     case GET_QUESTION:
-      return {
-        ...action.payload,
-      };
+      return  action.payload;
+    case RESET_QUESTION:
+      return initialState;
     default:
       return state;
   }
